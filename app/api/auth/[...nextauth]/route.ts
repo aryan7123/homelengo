0fb865aed901c43ac4dbc0f1c5b15dd630d0a6a8
EbadurRahman7 <aryanrahman90277@gmail.com>
2025-02-18 23:47:37 +0530

    fixing the next auth logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,6 +7,7 @@ import { compare } from 'bcryptjs';
 const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
