2515cf949bba6bb77592d296336c3f7e367b68b1
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-05-04 23:07:55 +0530

    created a route folder for fetching the total average review

---
+++ app/api/add-total-reviews/route.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest) {
+    try {
+        const response = await request.json();
+        const { userId } = response;
+
+        const getReviews = await prisma.review.findMany({
+            where: {
+                userId: Number(userId)
+            }
+        });
+
+        const totalRating = getReviews.map((item) => item.rating);
+        const averageRating = (totalRating.reduce((a, b) => a + b)) / 2;
+
+        return NextResponse.json({
+            sucess: true,
+            ratings: averageRating,
+            message: "Review fetched successfully",
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
