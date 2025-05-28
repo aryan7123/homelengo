1292a5a67350dafcf6eeffcee5fba9926e964b30
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-05-28 23:17:26 +0530

    created the functionality for liking a property

--- app/api/like-property/route.ts
+++ app/api/like-property/route.ts
@@ -6,12 +6,21 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
-    try {
-        const response = await request.json();
-        const { propertyId, userId } = response;
+  try {
+    const response = await request.json();
+    const { propertyId, userId } = response;
 
-        
-    } catch (error) {
-        console.log(error);
-    }
+    const likePropertyFn = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        likedProperties: {
+          connect: { id: propertyId },
+        },
+      },
+    });
+
+    return NextResponse.json({ likePropertyFn: likePropertyFn, message: "You liked this property!" });
+  } catch (error) {
+    console.log(error);
+  }
 }
