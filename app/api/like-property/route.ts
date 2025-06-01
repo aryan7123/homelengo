3139813c1403b88c555b01d8ca8d453d68c875d0
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-01 22:54:46 +0530

    converted the value of both to integer

--- app/api/like-property/route.ts
+++ app/api/like-property/route.ts
@@ -7,20 +7,29 @@ const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
   try {
-    const response = await request.json();
-    const { propertyId, userId } = response;
+    const body = await request.json();
 
-    const likePropertyFn = await prisma.user.update({
-      where: { id: userId },
-      data: {
-        likedProperties: {
-          connect: { id: propertyId },
+    if (!body || typeof body !== 'object') {
+      return NextResponse.json({ message: "Invalid or missing JSON body" }, { status: 400 });
+    }
+
+    const { propertyId, userId } = body;
+
+    if(propertyId && userId) {
+      const likePropertyFn = await prisma.user.update({
+        where: { id: Number(userId) },
+        data: {
+          likedProperties: {
+            connect: { id: Number(propertyId) },
+          },
         },
-      },
-    });
-
-    return NextResponse.json({ likePropertyFn: likePropertyFn, message: "You liked this property!" });
+      });
+      return NextResponse.json({ likePropertyFn: likePropertyFn, message: "You liked this property!" });
+    }
+    else {
+      return NextResponse.json({ message: "You must login to your account to like a property" });
+    }
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
