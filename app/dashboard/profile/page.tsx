ca7f14055da33a9b8484039fba2738cca49e92c2
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-12 18:56:39 +0530

    designing the my profile page

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div className="max-w-7xl mx-auto md:px-[120px] px-6 transition-[padding] duration-300 py-10">
+        <div className="bg-white w-full shadow-md rounded-2xl p-6 mt-8">
+          <h3 className="text-[#161e2d] mb-7 text-2xl font-semibold">
+            Account Settings
+          </h3>
+          <form className="w-full">
+            <div className="flex flex-col gap-2 mb-7">
+              <label className="text-xs font-semibold" htmlFor="full_name">Full Name: *</label>
+              <input className="w-full border border-[#e4e4e4] pl-4 py-2.5 rounded-[99px] bg-white text-[#161e2d] font-medium text-sm outline-none focus:border-[#1563df]" type="text" name="full_name" id="full_name" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="text-xs font-semibold" htmlFor="description">Description: *</label>
+              <textarea className="w-full h-28 resize-none border border-[#e4e4e4] pl-4 py-2.5 rounded-2xl bg-white text-[#161e2d] font-medium text-sm outline-none focus:border-[#1563df]" name="description" id="description" />
+            </div>
+            <div className="w-full mt-7 flex md:flex-row md:gap-0 gap-2 flex-col md:items-center items-start justify-between">
+              <div className="w-[inherit] flex flex-col gap-2">
+                <label className="text-xs font-semibold" htmlFor="occupation">Occupation: *</label>
+                <input className="md:w-[320px] w-full border border-[#e4e4e4] pl-4 py-2.5 rounded-[99px] bg-white text-[#161e2d] font-medium text-sm outline-none focus:border-[#1563df]" type="text" name="occupation" id="occupation" />
+              </div>
+              <div className="w-[inherit] flex flex-col gap-2">
+                <label className="text-xs font-semibold" htmlFor="phone_number">Phone Number: *</label>
+                <input className="md:w-[320px] w-full border border-[#e4e4e4] pl-4 py-2.5 rounded-[99px] bg-white text-[#161e2d] font-medium text-sm outline-none focus:border-[#1563df]" type="tel" name="phone_number" id="phone_number" />
+              </div>
+              <div className="w-[inherit] flex flex-col gap-2">
+                <label className="text-xs font-semibold" htmlFor="address">Address: *</label>
+                <input className="md:w-[320px] w-full border border-[#e4e4e4] pl-4 py-2.5 rounded-[99px] bg-white text-[#161e2d] font-medium text-sm outline-none focus:border-[#1563df]" type="text" name="address" id="address" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
