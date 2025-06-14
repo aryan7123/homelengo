e7b17b16790a66f7187797b722f28d593ba67c0b
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-14 18:21:49 +0530

    fixing the issue while updating the user details

--- app/api/account-details/route.ts
+++ app/api/account-details/route.ts
@@ -6,9 +6,36 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
-    try {
-        
-    } catch (error) {
-        console.log(error);
+  try {
+    const body = await req.json();
+    const { userId, fullName, description, occupation, phoneNumber, address } = body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(userId),
+      },
+    });
+
+    if (user) {
+      const updateUserDetails = await prisma.user.update({
+        where: {
+            id: Number(userId)
+        },
+        data: {
+            fullName: fullName,
+            description: description,
+            occupation: occupation,
+            phoneNumber: Number(phoneNumber),
+            address: address
+        }
+      });
+
+      return NextResponse.json({ updateUserDetails, message: "Details updated sucessfully" });
     }
+    else {
+        return NextResponse.json({ message: "Details cannot be upadted" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
