53619e196445e7e461b18fa12070949a729b5c24
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-14 14:08:38 +0530

    added some more columns to the user table

---
+++ prisma/migrations/20250614083519_fields_added_to_user/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "address" VARCHAR(255),
+ADD COLUMN     "description" TEXT,
+ADD COLUMN     "fullName" VARCHAR(255),
+ADD COLUMN     "occupation" VARCHAR(255),
+ADD COLUMN     "phoneNumber" INTEGER;
