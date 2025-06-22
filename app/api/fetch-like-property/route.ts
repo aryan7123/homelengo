002c37c7495ee69a1fe7e5c68c747099e5c89ef5
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-22 15:59:23 +0530

    fixed the property like check condition

--- app/api/fetch-like-property/route.ts
+++ app/api/fetch-like-property/route.ts
@@ -10,30 +10,53 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const { userId, propertyId } = body;
 
-    const user = await prisma.user.findUnique({
-      where: { id: Number(userId) },
-      select: {
-        likedProperties: {
-          select: { id: true },
+    if(userId) {
+      const user = await prisma.user.findUnique({
+        where: { id: Number(userId) },
+        include: {
+          likedProperties: true,
         },
-      },
-    });
-
-    const hasLiked = user?.likedProperties.some(
-      (property) => property.id === Number(propertyId)
-    );
-
-    if (hasLiked) {
-      return NextResponse.json({
-        liked: true,
-        likedProperties: user?.likedProperties,
-        message: "You have liked this property",
       });
-    } else {
-      return NextResponse.json({
-        liked: false,
-        message: "You have not liked this property",
+
+      if(user) {
+        return NextResponse.json({
+          likedProperties: user?.likedProperties,
+          message: "All liked properties by a user is fetched",
+        });
+      }
+      else {
+        return NextResponse.json({
+          message: "Liked properties is not fetched",
+        });
+      }
+    }
+    else if(userId && propertyId) {
+      const user = await prisma.user.findUnique({
+        where: { id: Number(userId) },
+        select: {
+          likedProperties: {
+            select: { id: true },
+          },
+        },
       });
+
+      const hasLiked = user?.likedProperties.some(
+        (property) => property.id === Number(propertyId)
+      );
+
+      if(hasLiked) {
+        return NextResponse.json({
+          liked: true,
+          likedProperties: user?.likedProperties,
+          message: "You have liked this property",
+        });
+      }
+      else {
+        return NextResponse.json({
+          liked: false,
+          message: "You have not liked this property",
+        });
+      }
     }
   } catch (error) {
     console.error(error);
