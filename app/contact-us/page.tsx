6284148686e16f86d583a3961c8bbd1e59269a31
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-23 18:57:19 +0530

    developing the contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Link from "next/link";
+import { FacebookIcon, InstagramIcon, LinkedinIcon, TwitterIcon, YoutubeIcon,  } from 'lucide-react';
 
 const page = () => {
   return (
@@ -13,12 +14,73 @@ const page = () => {
         <div className="max-w-7xl mx-auto px-6 py-12 md:px-0">
           <div className="relative contact-page-title">
             <div className="absolute inset-0 flex flex-col items-center justify-center text-white z-10">
-              <div className="flex items-center justify-center gap-1">
+              <div className="flex text-sm items-center justify-center gap-1">
                 <Link href="/">Home</Link> /
                 <span>Pages</span> /
                 <span>Contact Us</span>
               </div>
-              <h2 className="text-4xl md:text-[56px] font-semibold mt-3">Contact Us</h2>
+              <h2 className="text-4xl md:text-[56px] font-medium mt-4">Contact Us</h2>
+            </div>
+          </div>
+          <div className="w-[inherit] flex md:flex-row flex-col md:items-start items-center md:justify-between justify-start md:py-24 py-10 gap-8">
+            <div className="w-full md:w-[70%]">
+              <h2 className="text-[#161e2d] text-3xl mb-3">Drop Us A Line</h2>
+              <span className="text-[#5c6368] mt-3">Feel free to connect with us through our online channels for updates, news, and more.</span>
+              <form action="" className="mt-4">
+                <div className="w-full flex md:flex-row flex-col gap-8">
+                  <div className="md:w-1/2 w-full flex flex-col gap-1">
+                    <label className="text-sm text-[#161e2d] font-semibold mb-1" htmlFor="full_name">Full Name:</label>
+                    <input className="w-full rounded-3xl text-sm py-3 pl-3 text-[#161e2d] font-medium outline-none border border-[#e4e4e4]" type="text" name="full_name" id="full_name" />
+                  </div>
+                  <div className="md:w-1/2 w-full flex flex-col gap-1">
+                    <label className="text-sm text-[#161e2d] font-semibold mb-1" htmlFor="email_address">Email Address:</label>
+                    <input className="w-full rounded-3xl text-sm py-3 pl-3 text-[#161e2d] font-medium outline-none border border-[#e4e4e4]" type="email" name="email_address" id="email_address" />
+                  </div>
+                </div>
+                <div className="w-full flex md:flex-row flex-col gap-8 mt-6">
+                  <div className="md:w-1/2 w-full flex flex-col gap-1">
+                    <label className="text-sm text-[#161e2d] font-semibold mb-1" htmlFor="phone_number">Phone Number:</label>
+                    <input className="w-full rounded-3xl text-sm py-3 pl-3 text-[#161e2d] font-medium outline-none border border-[#e4e4e4]" type="tel" name="phone_number" id="phone_number" />
+                  </div>
+                  <div className="md:w-1/2 w-full flex flex-col gap-1">
+                    <label className="text-sm text-[#161e2d] font-semibold mb-1" htmlFor="subject">Subject:</label>
+                    <input className="w-full rounded-3xl text-sm py-3 pl-3 text-[#161e2d] font-medium outline-none border border-[#e4e4e4]" type="text" name="subject" id="subject" />
+                  </div>
+                </div>
+                <div className="w-full flex flex-col gap-1 mt-6">
+                  <label className="text-sm text-[#161e2d] font-semibold mb-1" htmlFor="">Your Message:</label>
+                  <textarea className="w-full h-28 rounded-lg text-sm py-3 pl-3 text-[#161e2d] font-medium outline-none border border-[#e4e4e4]" name="message" id="message"></textarea>
+                </div>
+                <button type="button" className="w-full bg-[#1563df] text-white font-medium text-base rounded-full mt-6 py-3.5 transition-colors hover:bg-[#0e49a6]">Send Message</button>
+              </form>
+            </div>
+            <div className="w-full md:w-[30%] py-7 px-7 border border-[#e4e4e4] rounded-2xl">
+              <h2 className="text-[#161e2d] text-3xl">Contact Us</h2>
+              <div className="mt-8 mb-4">
+                <h3 className="text-[#161e2d] mb-3 font-semibold text-base">Address:</h3>
+                <p className="text-[#5c6368] text-sm">101 E 129th St, East Chicago, IN 46312</p>
+                <p className="text-[#5c6368] text-sm">United States</p>
+              </div>
+              <div className="mt-8 mb-4">
+                <h3 className="text-[#161e2d] mb-3 font-semibold text-base">Information:</h3>
+                <p className="text-[#5c6368] text-sm">1-333-345-6868</p>
+                <p className="text-[#5c6368] text-sm">hi.themesflat@gmail.com</p>
+              </div>
+              <div className="mt-8 mb-4">
+                <h3 className="text-[#161e2d] mb-3 font-semibold text-base">Timing:</h3>
+                <p className="text-[#5c6368] text-sm">Monday - Friday: 08:00 - 20:00</p>
+                <p className="text-[#5c6368] text-sm">Saturday - Sunday: 10:00 - 18:00</p>
+              </div>
+              <div className="mt-8 mb-4">
+                <h3 className="text-[#161e2d] mb-3 font-semibold text-base">Follow Us:</h3>
+                <div className="flex items-center gap-2.5">
+                  <button type="button" className="w-11 h-11 rounded-lg flex items-center justify-center border border-[#e4e4e4] transition-colors hover:bg-[#1563df] hover:text-white"><FacebookIcon /></button>
+                  <button type="button" className="w-11 h-11 rounded-lg flex items-center justify-center border border-[#e4e4e4] transition-colors hover:bg-[#1563df] hover:text-white"><InstagramIcon /></button>
+                  <button type="button" className="w-11 h-11 rounded-lg flex items-center justify-center border border-[#e4e4e4] transition-colors hover:bg-[#1563df] hover:text-white"><YoutubeIcon /></button>
+                  <button type="button" className="w-11 h-11 rounded-lg flex items-center justify-center border border-[#e4e4e4] transition-colors hover:bg-[#1563df] hover:text-white"><TwitterIcon /></button>
+                  <button type="button" className="w-11 h-11 rounded-lg flex items-center justify-center border border-[#e4e4e4] transition-colors hover:bg-[#1563df] hover:text-white"><LinkedinIcon /></button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
