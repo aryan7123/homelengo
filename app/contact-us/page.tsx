716f82c14fcfc7ab1922d99383d1fc54cb6518ea
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-26 22:22:29 +0530

    added some faqs

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -270,25 +270,17 @@ const page = () => {
               type="single"
               collapsible
               className="w-full mt-10"
-              defaultValue="item-1"
             >
               <AccordionItem
                 className="border border-[#e4e4e4] rounded-2xl py-1 px-6 mb-3 data-[state=open]:shadow-md"
                 value="item-1"
               >
                 <AccordionTrigger className="text-xl text-[#161e2d] font-semibold data-[state=open]:text-blue-600">
-                  Product Information
+                  What types of accommodation are available in the USA for long-term stays?
                 </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-2 text-balance">
+                <AccordionContent className="flex flex-col gap-2">
                   <p className="text-sm text-[#5c6368] font-medium">
-                    Our flagship product combines cutting-edge technology with
-                    sleek design. Built with premium materials, it offers
-                    unparalleled performance and reliability.
-                  </p>
-                  <p className="text-sm text-[#5c6368] font-medium">
-                    Key features include advanced processing capabilities, and
-                    an intuitive user interface designed for both beginners and
-                    experts.
+                    In the USA, long-term accommodation options include apartments, single-family homes, condominiums, townhouses, and shared housing. Many areas also offer furnished corporate rentals and extended-stay hotels for professionals or temporary residents.
                   </p>
                 </AccordionContent>
               </AccordionItem>
@@ -297,38 +289,57 @@ const page = () => {
                 value="item-2"
               >
                 <AccordionTrigger className="text-xl text-[#161e2d] font-semibold data-[state=open]:text-blue-600">
-                  Shipping Details
+                  What is the average rent in major U.S. cities?
                 </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-2 text-balance">
+                <AccordionContent className="flex flex-col gap-2">
                   <p className="text-sm text-[#5c6368] font-medium">
-                    We offer worldwide shipping through trusted courier
-                    partners. Standard delivery takes 3-5 business days, while
-                    express shipping ensures delivery within 1-2 business days.
+                    <li className="mb-1">New York City: $2,500 – $4,000/month for a 1-bedroom</li>
+                    <li className="mb-1">Los Angeles: $2,200 – $3,500/month</li>
+                    <li className="mb-1">Chicago: $1,500 – $2,800/month</li>
+                    <li>Suburban and smaller-town areas are generally more affordable than coastal metro regions.</li>
                   </p>
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem
+                className="border border-[#e4e4e4] rounded-2xl mb-3 py-1 px-6 data-[state=open]:shadow-md"
+                value="item-3"
+              >
+                <AccordionTrigger className="text-xl text-[#161e2d] font-semibold data-[state=open]:text-blue-600">
+                  Do I need a U.S. credit history to rent a property?
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-2">
                   <p className="text-sm text-[#5c6368] font-medium">
-                    All orders are carefully packaged and fully insured. Track
-                    your shipment in real-time through our dedicated tracking
-                    portal.
+                   While a credit history helps, it’s not always required. Landlords may accept a larger security deposit, a co-signer, or proof of income or employment, especially for international renters or new arrivals.
+                  </p>
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem
+                className="border border-[#e4e4e4] rounded-2xl mb-3 py-1 px-6 data-[state=open]:shadow-md"
+                value="item-4"
+              >
+                <AccordionTrigger className="text-xl text-[#161e2d] font-semibold data-[state=open]:text-blue-600">
+                  What’s the process of buying a house in the USA as a non-resident?
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-2">
+                  <p className="text-sm text-[#5c6368] font-medium">
+                    <li className="mb-1">Hiring a local real estate agent</li>
+                    <li className="mb-1">Getting pre-approved for financing (if needed)</li>
+                    <li className="mb-1">Making an offer and signing a purchase agreement</li>
+                    <li>Completing legal and financial due diligence</li>
+                    <li>Closing with a title company or attorney</li>
                   </p>
                 </AccordionContent>
               </AccordionItem>
               <AccordionItem
                 className="border border-[#e4e4e4] rounded-2xl py-1 px-6 data-[state=open]:shadow-md"
-                value="item-3"
+                value="item-5"
               >
                 <AccordionTrigger className="text-xl text-[#161e2d] font-semibold data-[state=open]:text-blue-600">
-                  Return Policy
+                  Are short-term rentals like Airbnb legal in all U.S. cities?
                 </AccordionTrigger>
-                <AccordionContent className="flex flex-col gap-2 text-balance">
+                <AccordionContent className="flex flex-col gap-2">
                   <p className="text-sm text-[#5c6368] font-medium">
-                    We stand behind our products with a comprehensive 30-day
-                    return policy. If you&apos;re not completely satisfied, simply
-                    return the item in its original condition.
-                  </p>
-                  <p className="text-sm text-[#5c6368] font-medium">
-                    Our hassle-free return process includes free return shipping
-                    and full refunds processed within 48 hours of receiving the
-                    returned item.
+                    Not always. Rules vary by city and state. Some cities (e.g. San Francisco, New York, Los Angeles) have strict regulations on short-term rentals. It’s important to check local laws and obtain permits or licenses if you plan to rent out a property.
                   </p>
                 </AccordionContent>
               </AccordionItem>
