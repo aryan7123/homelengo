bfebf8e459a9a7cb6a3054b4825ad12a635a9728
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-06-30 14:14:46 +0530

    created the footer hosting section in the contact page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Image from "next/image";
 import Link from "next/link";
 import axios from "axios";
 import {
@@ -11,6 +12,7 @@ import {
   LinkedinIcon,
   TwitterIcon,
   YoutubeIcon,
+  ArrowRight
 } from "lucide-react";
 import {
   Accordion,
@@ -344,6 +346,24 @@ const page = () => {
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
+          </div>
+          <div className="w-full my-20 bg-[#f3f7fd] rounded-2xl relative">
+            <div className="flex md:flex-row flex-col md:items-center items-start md:justify-between md:gap-0 gap-10 justify-start">
+              <div className="md:pl-20 pl-6 md:pt-0 pt-10 pr-5">
+                <span className="uppercase text-sm font-medium text-[#1563df]">become partners</span>
+                <h3 className="md:text-4xl text-2xl mt-4 text-[#161e2d] text-balance font-semibold">List your Properties on Homelengo, join Us Now!</h3>
+                <button type="button" className="w-[250px] flex items-center justify-center gap-1 bg-[#1563df] text-white font-semibold text-base rounded-full mt-6 py-3.5 transition-colors hover:bg-[#0e49a6] capitalize">become a hosting <ArrowRight size={20}/> </button>
+              </div>
+              <div>
+                <Image
+                  className="scale-100 md:mt-[-60px] mt-0"
+                  src="/banner/banner.png"
+                  alt="banner"
+                  width={1200}
+                  height={300}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </section>
