7422245b3f16530671ba23a2b2a6748f04d6b449
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-07-02 21:45:15 +0530

    created an api route to get all the reviews

---
+++ app/api/all-reviews/route.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const reviews = await prisma.review.findMany({
+      take: 5,
+      select: { rating: true, comment: true, username: true },
+    });
+
+    if(reviews.length > 0) {
+        return NextResponse.json({
+            reviews: reviews,
+            message: "Reviews successfully found"
+        });
+    }
+    else {
+        return NextResponse.json({
+            message: "No Reviews Found"
+        });
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+}
