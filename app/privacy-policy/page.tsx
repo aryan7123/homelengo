23913c640aa9e30e854bd4a4726685644233ba3e
Raza Ali Rahman <aryanrahman90277@gmail.com>
2025-07-21 22:03:38 +0530

    feat: created a new page featuring privacy and policy

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import ScrollUpButton from "../components/ScrollUpButton";
+import Footer from "../components/Footer";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <ScrollUpButton />
+      <Footer />
+    </>
+  );
+};
+
+export default page;
